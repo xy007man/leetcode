a3e1b3e5129f2fd549159b50b931a789dbbbef9b
xy007man <1219070362@qq.com>
2018-06-17 21:59:04 +0800

    lintcode练习

---
+++ 006_LongestConsecutiveSequence/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int longestConsecutive(const vector<int> &nums) {
+
+    unordered_map<int, bool> used;
+    int longest = 0;
+
+    for (auto i : nums) {
+        used[i] = false;
+    }
+
+    for (auto i : nums) {
+
+        if (used[i])
+            continue;
+
+        int length = 1;
+
+        for (int j=i+1; used.find(j) != used.end(); j++) {
+            used[i] = true;
+            length++;
+        }
+        for (int j=i-1; used.find(j) != used.end(); j--) {
+            used[i] = true;
+            length++;
+        }
+
+        longest = length > longest ? length : longest;
+    }
+
+    return longest;
+
+}
+
+int main()
+{
+    int a[] = {100, 4, 200, 1, 3, 2};
+
+    vector<int> nums(a, a+6);
+
+    cout << longestConsecutive(nums) << endl;
+
+    return 0;
+}
---
+++ 007_Two Sum/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target) {
+
+    unordered_map<int, int> map;
+    vector<int> res;
+
+    for (int i=0; i<nums.size(); i++) {
+        map[nums[i]] = i;
+    }
+
+    for (int i=0; i<nums.size(); i++) {
+        int gap = target - nums[i];
+        if (map.find(gap) != map.end() && map[gap] > i) {
+            res.push_back(i + 1);
+            res.push_back(map[gap] + 1);
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+
+    int a[] = {100, 4, 200, 1, 3, 2};
+
+    vector<int> nums(a, a+6);
+
+    vector<int> res = twoSum(nums, 7);
+
+    for (int i=0; i<res.size(); i++) {
+        cout << res[i] << " ";
+    }
+
+    return 0;
+}
